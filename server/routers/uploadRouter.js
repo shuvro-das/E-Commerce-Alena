18e2fa5ea9cc48e3e9cfce4a2ec9db6446e7b96b
shuvro-das <shuvrodas1999@gmail.com>
2021-05-26 10:24:17 +0530

    eleven commit

--- server/routers/uploadRouter.js
+++ server/routers/uploadRouter.js
@@ -7,7 +7,7 @@ const maxSize = 1 * 1024 * 1024; // 1 mb
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, "/uploads/");
   },
 
   filename(req, file, cb) {
